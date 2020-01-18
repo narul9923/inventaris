7f4beb9b7485fc0ad78173145934720c46bc1477
root <narulhidayah9923@gmail.com>
2020-01-18 23:06:04 +0700

    fix login and check validation

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -3,8 +3,37 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Login extends CI_Controller {
 
+    function __construct(){
+        parent::__construct();
+        $this->load->model('m_login');
+    }
+
     public function index(){
         $this->load->view('login');
+    }
+
+    public function cek_login(){
+        $username = $this->input->post('username');
+        $password = $this->input->post('password');
+
+        $where = array(
+            'username' => $username,
+            'password' => md5($password),
+        );
+        $cek = $this->m_login->cek_login($where);
+        if($cek->num_rows() != 0){
+            $data = $cek->row();
+            if($data->id_level = '1'){
+                echo "halaman admin";
+            }elseif($data->id_level = '2'){
+                echo "halaman pegawai";
+            }elseif($data->id_level = '3'){
+                echo "halaman peminjam";
+            }
+        }else{
+            $this->session->set_flashdata('error','Username atau password salah');
+            redirect(base_url('login'));
+        }
     }
 
 }
--- application/views/login.php
+++ application/views/login.php
@@ -6,16 +6,26 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     <link rel="stylesheet" href="<?php echo base_url('assets/dist/css/adminlte.css') ?>">
-    <script src="<?php echo base_url('assets/dist/js/adminlte.css') ?>"></script>
+    <script src="<?php echo base_url('assets/plugins/jquery/jquery.slim.js') ?>"></script>
+    <script src="<?php echo base_url('assets/dist/js/adminlte.js') ?>"></script>
 </head>
 <body>
     <div class="container">
         <div class="card" style="width: 25rem;margin:auto;margin-top:15%;">
             <div class="card-body">
+                <?php
+                if($this->session->flashdata('error')){
+                  ?>
+                    <div class="alert alert-danger fade show" role="alert">
+                        <?php echo $this->session->flashdata('error'); ?>
+                    </div>
+                <?php  
+                }
+                ?>
                 <form action="<?php echo base_url('login/cek_login') ?>" method="post">
                     <div class="form-group">
-                        <label>Email address</label>
-                        <input name="email" type="email" class="form-control" placeholder="Enter email">
+                        <label>Username</label>
+                        <input name="username" type="text" class="form-control" placeholder="Enter email">
                     </div>
                     <div class="form-group">
                         <label>Password</label>
